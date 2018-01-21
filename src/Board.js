749f835b614928880a07bfad1d6a9a2536f5fc15
Roshan <roshan.lobo@gmail.com>
2018-01-21 18:07:05 +1100

    Complete game of tic tac toe

--- src/Board.js
+++ src/Board.js
@@ -8,6 +8,7 @@ class Board extends React.Component {
         this.state = {
             squares: Array(9).fill(null),
             xIsNext: true,
+            status: 'Next player: X',
         };
     }
 
@@ -19,14 +20,46 @@ class Board extends React.Component {
     }
 
     handleClick = (i) => {
-        this.setState(this.state.squares.splice(i, 1, 'X'));
+        let squares = this.state.squares.slice();
+        let status = '';
+        if (squares[i] === null) {
+            squares[i] = (this.state.xIsNext ? 'X' : 'O');
+            if (this.hasPlayerWon(squares)) {
+                status = `Player ${(this.state.xIsNext ? 'X' : 'O')} won`;
+            } else {
+                status = `Next player ${(!this.state.xIsNext ? 'X' : 'O')}`;
+            }
+            this.setState({
+                squares,
+                xIsNext: !this.state.xIsNext,
+                status,
+            });
+        }
+    };
+
+    hasPlayerWon = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return true;
+            }
+        }
     };
 
     render() {
-        const status = 'Next player: X';
         return (
             <div>
-                <div className="status">{status}</div>
+                <div className="status">{this.state.status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
